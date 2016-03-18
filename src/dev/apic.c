caf8fc0dd30a3ddcc1086e792063026020325c23
Chris beauchene <cbeauchene93@gmail.com>
2016-03-18 10:49:17 -0500

    Update Apic

--- src/dev/apic.c
+++ src/dev/apic.c
@@ -373,9 +373,9 @@ void apic_deadline_write(struct apic_dev *apic, uint64_t cycles)
 #define TIME_TO_APIC_CYCLES  42
 
 void set_apic_oneshot(struct apic_dev *apic, uint32_t time_us)
-{
-	apic_write(apic, APIC_REG_TMICT, time_us * TIME_TO_APIC_CYCLES);
+{	
 	apic_write(apic, APIC_REG_LVTT, 0 | APIC_DEL_MODE_FIXED | APIC_TIMER_INT_VEC | APIC_TIMER_ONESHOT);
+	apic_write(apic, APIC_REG_TMICT, time_us * TIME_TO_APIC_CYCLES);
 }
 
 int apic_oneshot_read(struct apic_dev *apic)
